da46aecb2c64ea5a52fcde8561d0d349edbfee6d
OWENSLAY <136379654+OWENSLAY@users.noreply.github.com>
2025-05-10 14:32:00 -0400

    initialisation du capteur de proximite

--- src/Proximite.cpp
+++ src/Proximite.cpp
@@ -1,2 +1,28 @@
 #include "Proximite.h"
 
+
+
+SparkFun_APDS9960 adps = SparkFun_APDS9960();
+
+
+
+
+
+
+
+
+
+void setup (){
+   // Wire.begin(SDA, SCL, APDS9960_I2C_ADDR); // join l'adresse du  slave au bus i2c
+  //  Wire.onRequest(requestEvent);
+  //Initialise le capteur de proximité  
+ adps.init();
+    adps.setProximityGain(PGAIN_2X);
+    adps.enableProximitySensor(true);
+    adps.enableLightSensor(true);
+    
+
+}
+ 
+
+ 
--- src/Proximite.h
+++ src/Proximite.h
@@ -1,1 +1,6 @@
 #include <Arduino.h>
+#include <Wire.h>
+#include <SparkFun_APDS9960.h>
+
+
+
