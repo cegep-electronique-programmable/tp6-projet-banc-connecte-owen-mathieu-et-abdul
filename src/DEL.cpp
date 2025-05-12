6c0dedc8a0ed6de89ee70c9f44b170e7685bc004
BeyondAtomic <asaboor.amj@gmail.com>
2025-05-12 13:55:12 -0400

    del_v.1

--- src/DEL.cpp
+++ src/DEL.cpp
@@ -1,1 +1,24 @@
 #include "DEL.h"
+
+void setup() {
+  // Initialiser les LEDs et les capteurs
+  pinMode(LED_BUILTIN, OUTPUT);
+  run();
+  Serial.begin(9600); // Pour déboguer et afficher des informations
+}
+
+void loop() {
+  setBrightness(); // Ajuster la luminosité selon l'environnement
+
+  // Simuler l'état de charge
+  bool isCharging = true; // Mettre à jour cette variable en fonction de l'état réel du chargeur
+
+  // Si l'appareil est en charge, allumer les LEDs en rouge, sinon en jaune
+  if (isCharging) {
+    rouge(); // Charge active
+  } else {
+    jaune(); // Pas en charge
+  }
+
+  delay(1000); // Attendre 1 seconde avant la prochaine mise à jour
+}
