ef23464526cd9c2058ed11802e1af74961f4453c
BeyondAtomic <asaboor.amj@gmail.com>
2025-05-21 18:28:11 -0400

    del moidf back to v1

--- src/DEL.cpp
+++ src/DEL.cpp
@@ -9,7 +9,7 @@
 
 
 Adafruit_NeoPixel strip(10, PIN, NEO_GRB + NEO_KHZ800);
-//Adafruit_NeoPixel pixels(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
+Adafruit_NeoPixel pixels(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
 
 #define DELAYVAL 1
 
